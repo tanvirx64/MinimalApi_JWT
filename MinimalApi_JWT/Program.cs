54c60590381f131b862f429445070bdb402e5c55
Tanvir Ahmed <tanvircsdev@gmail.com>
2024-02-04 00:04:52 +0600

    JWT authentication and Authorization is added

--- MinimalApi_JWT/Program.cs
+++ MinimalApi_JWT/Program.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MinimalApi_JWT.Models;
 using MinimalApi_JWT.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme { 
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Description = "Bearer Authentication with JWT Token",
+        Type = SecuritySchemeType.Http
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+});
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
     options.TokenValidationParameters = new TokenValidationParameters()
     {
@@ -26,18 +54,60 @@ builder.Services.AddSingleton<IUserService, UserService>();
 
 var app = builder.Build();
 
-app.UseAuthentication();
+app.UseAuthorization();
 app.UseAuthentication();
 app.UseSwagger();
 
 
-app.MapGet("/", () => "Hello World!");
-app.MapPost("/create", (Movie movie, IMovieService service) => Create(movie, service));
-app.MapGet("/get", (int id, IMovieService service) => Get(id, service));
+app.MapGet("/", () => "Hello World!").ExcludeFromDescription();
+app.MapPost("/login", (UserLogin user, IUserService service) => Login(user, service));
+app.MapPost("/create",
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+    (Movie movie, IMovieService service) => Create(movie, service));
+app.MapGet("/get",
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Standard, Administrator")]
+(int id, IMovieService service) => Get(id, service));
 app.MapGet("/list", (IMovieService service) => GetAll(service));
-app.MapPut("/update", (Movie movie , IMovieService service) => Update(movie, service));
-app.MapDelete("/delete", (int id, IMovieService service) => Delete(id, service));
+app.MapPut("/update",
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+    (Movie movie , IMovieService service) => Update(movie, service));
+app.MapDelete("/delete",
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+    (int id, IMovieService service) => Delete(id, service));
 
+IResult Login(UserLogin user, IUserService service)
+{
+    if(!string.IsNullOrEmpty(user.Username) && !string.IsNullOrEmpty(user.Password))
+    {
+        User loggedInUser = service.Get(user);
+        if (loggedInUser is null) return Results.NotFound("User Not Found!");
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, loggedInUser.Username),
+            new Claim(ClaimTypes.Email, loggedInUser.EmailAddress),
+            new Claim(ClaimTypes.GivenName, loggedInUser.GivenName),
+            new Claim(ClaimTypes.Surname, loggedInUser.Surname),
+            new Claim(ClaimTypes.Role, loggedInUser.Role),
+        };
+        var token = new JwtSecurityToken(
+            issuer: builder.Configuration["Jwt:Issuer"],
+            audience: builder.Configuration["Jwt:Audience"],
+            claims: claims,
+            notBefore: DateTime.UtcNow,
+            expires: DateTime.UtcNow.AddDays(60),
+            signingCredentials: new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])), 
+                SecurityAlgorithms.HmacSha256)
+        );
+
+        var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return Results.Ok(tokenString);
+    }
+
+    return Results.BadRequest("Username or Password is missing!");
+}
 IResult Create(Movie movie, IMovieService service)
 {
     return Results.Ok(service.Create(movie));
