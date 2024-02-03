03f84c7f8feb137a7ece2878ca69b3a5fb15f3cc
Tanvir Ahmed <tanvircsdev@gmail.com>
2024-02-03 15:44:47 +0600

    Repositories are added

---
+++ MinimalApi_JWT/Repositories/MovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using MinimalApi_JWT.Models;
+
+namespace MinimalApi_JWT.Repositories
+{
+    public class MovieRepository
+    {
+        public static List<Movie> movies = new List<Movie>() 
+        {
+            new() { Id = 1, Title = "Inception", Description = "Mind-bending thriller", Rating = 4.5 },
+            new() { Id = 2, Title = "The Shawshank Redemption", Description = "Prison drama", Rating = 4.8 },
+            new() { Id = 3, Title = "The Dark Knight", Description = "Superhero action", Rating = 4.7 },
+            new() { Id = 4, Title = "Pulp Fiction", Description = "Quentin Tarantino classic", Rating = 4.6 },
+            new() { Id = 5, Title = "Forrest Gump", Description = "Heartwarming drama", Rating = 4.4 }
+        };
+    }
+}
---
+++ MinimalApi_JWT/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using MinimalApi_JWT.Models;
+
+namespace MinimalApi_JWT.Repositories
+{
+    public class UserRepository
+    {
+        public static List<User> users= new List<User>() 
+        { 
+            new (){ Username="tanvir_admin",EmailAddress="tanvir@gmail.com", GivenName="Tanvir", Surname="Ahmed", Password="MyPass_w0rd", Role="Administrator" },
+            new (){ Username="rakib_standard",EmailAddress="rakib@gmail.com", GivenName="Rakib", Surname="Rahman", Password="MyPass_w0rd", Role="Standard" },
+        };
+    }
+}
